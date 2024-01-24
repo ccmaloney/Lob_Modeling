026a571ef9335edf24977120f8b26162d6d49360
Charles Maloney <charles.c.maloney@gmail.com>
2024-01-23 21:29:55 -0600

    changes nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,5 @@
+# in flake.nix
 {
-  description = "My Python Environment with Jupyter Lab";
-
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-  };
-
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.${self.system};
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          pkgs.python38
-          pkgs.jupyterlab
-        ];
-      };
-    };
+  outputs = inputs: {};
 }
+
